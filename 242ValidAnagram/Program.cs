f5324677b34c480aea85658b91bc8d0cd55ac915
Serhii Shtokal <serhiishtokal@gmail.com>
2023-08-18 02:38:01 +0200

    242. Valid Anagram

---
+++ 242ValidAnagram/Program.cs
@@ -0,0 +1,32 @@
+﻿var s = "anagram";
+var t = "nagaram";
+
+var solution = new Solution();
+var result = solution.IsAnagram(s,t);
+Console.WriteLine(result);
+Console.ReadLine();
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if(s == t) return true;
+        if(s.Length !=  t.Length) return false;
+
+        var symbolFrequencyDictionary = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var symbolFromS = s[i];
+            var symbolFromT = t[i];
+
+            symbolFrequencyDictionary.TryAdd(symbolFromS, 0);
+            symbolFrequencyDictionary.TryAdd(symbolFromT, 0);
+
+            symbolFrequencyDictionary[symbolFromS]++;
+            symbolFrequencyDictionary[symbolFromT]--;
+        }
+
+        return symbolFrequencyDictionary.Values.All(x => x == 0);
+    }
+}
