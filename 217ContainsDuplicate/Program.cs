53417a35a85b2b08d2916cce3847534ed845887c
Serhii Shtokal <serhiishtokal@gmail.com>
2023-08-18 02:09:08 +0200

    217. Contains Duplicate

---
+++ 217ContainsDuplicate/Program.cs
@@ -0,0 +1,25 @@
+﻿var nums = new int[] { 1, 2, 3, 1 };
+var solution = new Solution();
+var result = solution.ContainsDuplicate(nums);
+Console.WriteLine(result);
+Console.ReadLine();
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var hash = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (hash.Contains(nums[i]))
+            {
+                return true;
+            }
+
+            hash.Add(nums[i]);
+        }
+
+        return false;
+    }
+}
