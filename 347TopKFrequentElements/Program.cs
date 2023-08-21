a3f826289d5b678c7ea84e5a68ebb80bd7a3c8f9
Serhii Shtokal <serhiishtokal@gmail.com>
2023-08-21 19:10:40 +0200

    347. Top K Frequent Elements

---
+++ 347TopKFrequentElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+var nums = new int[] { 1, 1, 1, 2, 2, 3 };
+var k = 2;
+var solution = new Solution();
+var result = solution.TopKFrequent(nums, k);
+string resultJson = JsonSerializer.Serialize(result);
+Console.WriteLine(resultJson);
+Console.ReadLine();
+
+public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        var dictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!dictionary.TryAdd(nums[i], 1))
+            {
+                dictionary[nums[i]]++;
+            }
+        }
+
+        return dictionary
+            .OrderByDescending(x => x.Value)
+            .Take(k)
+            .Select(x => x.Key)
+            .ToArray();
+    }
+}
